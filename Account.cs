444ee4ce72a159e496c408cef6ad4728d89e3525
Steve Smith <steve@kentsmiths.com>
2021-09-29 08:12:41 -0400

    final changes before publishing article

--- Account.cs
+++ Account.cs
@@ -6,6 +6,7 @@ public class Account
 {
     public Account(string name, string number, decimal balance)
     {
+        // comment these out to see the test failures and their messages
         Name = Guard.Against.NullOrEmpty(name, nameof(name));
         Number = Guard.Against.NullOrEmpty(number, nameof(number));
         Balance = Guard.Against.NegativeOrZero(balance, nameof(balance));
--- Account_Constructor.cs
+++ Account_Constructor.cs
@@ -1,3 +1,5 @@
+using FluentAssertions;
+using FluentAssertions.Execution;
 using Xunit;
 
 namespace MultipleAssertions;
@@ -49,9 +51,51 @@ public class Account_Constructor
             exceptions.Add(ex);
         }
 
-        if(exceptions.Any())
+        if (exceptions.Any())
         {
-            throw new AggregateException(exceptions.ToArray()); 
+            throw new AggregateException(exceptions.ToArray());
         }
     }
+
+    [Fact]
+    public void SetsAssociatedProperties_FluentAssertionsAssertionScopes_XUnitAsserts()
+    {
+        var account = new Account(_testName, _testNumber, _testBalance);
+
+        using (new AssertionScope())
+        {
+            // doesn't work with xUnit asserts
+            Assert.Equal(_testName, account.Name);
+            Assert.Equal(_testNumber, account.Number);
+            Assert.Equal(_testBalance, account.Balance);
+        }
+    }
+
+    [Fact]
+    public void SetsAssociatedProperties_FluentAssertionsAssertionScopes_FluentAsserts()
+    {
+        var account = new Account(_testName, _testNumber, _testBalance);
+
+        using (new AssertionScope())
+        {
+            account.Name.Should().Be(_testName);
+            account.Number.Should().Be(_testNumber);
+            account.Balance.Should().Be(_testBalance);
+        }
+    }
+
+    [Fact]
+    public void SetsAssociatedProperties_AssertAll()
+    {
+        var account = new Account(_testName, _testNumber, _testBalance);
+
+        var expectations = new List<Tuple<object, object>>()
+        {
+            new(_testName, account.Name),
+            new(_testName, account.Number),
+            new(_testBalance, account.Balance),
+        };
+        Assert.All(expectations, pair => Assert.Equal(pair.Item1, pair.Item2));
+    }
+
 }
